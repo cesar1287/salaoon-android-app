3456ace4334442684c366c7f85726f5963334ba6
Islane <islane.junior@gmail.com>
2017-10-19 14:10:28 -0300

    Resolvendo Conflitos

--- app/src/main/java/comcesar1287/github/salaoon/SplashScreenActivity.java
+++ app/src/main/java/comcesar1287/github/salaoon/SplashScreenActivity.java
@@ -15,7 +15,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreenActivity.this, CategoryRegisterActivity.class);
+                Intent i = new Intent(SplashScreenActivity.this, SignWithActivity.class);
                 startActivity(i);
                 finish();
             }
