025902f293e413971c68ecc83c527eb83f36e281
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2017-10-21 15:20:40 -0300

    Consertado pequenas coisas na tela de cadastro de profissional

--- app/src/main/java/comcesar1287/github/salaoon/view/RegisterProfessionalActivity.java
+++ app/src/main/java/comcesar1287/github/salaoon/view/RegisterProfessionalActivity.java
@@ -1,58 +1,64 @@
 package comcesar1287.github.salaoon.view;
 
 import android.app.TimePickerDialog;
-import android.icu.util.Calendar;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TimePicker;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 import comcesar1287.github.salaoon.R;
 
 public class RegisterProfessionalActivity extends AppCompatActivity {
 
     Button hourFirst, hourFinal;
-    int horaInicial, horaFinal, minutoInicial, minutoFinal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_professional);
 
-        hourFirst = (Button) findViewById(R.id.register_button_hour_first);
-        hourFinal = (Button) findViewById(R.id.register_button_hour_final);
-
+        hourFirst = findViewById(R.id.register_button_hour_first);
+        hourFinal = findViewById(R.id.register_button_hour_final);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
     public void btn_hour_first(View view) {
-        final Calendar c = Calendar.getInstance();
-        horaInicial=c.get(Calendar.HOUR_OF_DAY);
-        minutoInicial=c.get(Calendar.MINUTE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, Calendar.MONTH, Calendar.DATE, 8, 0);
 
         TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                hourFirst.setText(hourOfDay + ":" + minute);
+                Date date = new Date();
+                date.setMinutes(minute);
+                date.setHours(hourOfDay);
+                SimpleDateFormat format = new SimpleDateFormat("HH:mm", Locale.US);
+                String formatted = format.format(date);
+                hourFirst.setText(formatted);
             }
-            }, horaInicial, minutoInicial, false);
+            }, calendar.getTime().getHours(), calendar.getTime().getMinutes(), true);
 
         timePickerDialog.show();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
     public void btn_hour_final(View view) {
-        final Calendar c = Calendar.getInstance();
-        horaFinal=c.get(Calendar.HOUR_OF_DAY);
-        minutoFinal=c.get(Calendar.MINUTE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, Calendar.MONTH, Calendar.DATE, 18, 0);
 
         TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                hourFinal.setText(hourOfDay + ":" + minute);
+                Date date = new Date();
+                date.setMinutes(minute);
+                date.setHours(hourOfDay);
+                SimpleDateFormat format = new SimpleDateFormat("HH:mm", Locale.US);
+                String formatted = format.format(date);
+                hourFinal.setText(formatted);
             }
-        }, horaInicial, minutoInicial, false);
+        }, calendar.getTime().getHours(), calendar.getTime().getMinutes(), true);
 
         timePickerDialog.show();
     }
